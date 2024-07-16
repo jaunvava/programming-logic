f1a3bc52828fc80fc0b53e35d783043239cdc930
jaunvava <mortalteck1020@gmail.com>
2024-07-16 15:52:20 -0300

    update

--- Aula-typescript/src/index.ts
+++ Aula-typescript/src/index.ts
@@ -52,7 +52,11 @@ const pessoa1: Person = {
 type Criterio = "greater" | "lower";
 // apelido para o tipo
 // parametros opcionais somente no final da função
-function chooseNumber(numero1: number, numero2: number, criterio?: Criterio): number {
+function chooseNumber(
+  numero1: number,
+  numero2: number,
+  criterio?: Criterio
+): number {
   switch (criterio) {
     case "greater":
       return numero1 > numero2 ? numero1 : numero2;
@@ -60,7 +64,7 @@ function chooseNumber(numero1: number, numero2: number, criterio?: Criterio): nu
       return numero1 < numero2 ? numero1 : numero2;
     default:
       const numeroAleatorio = Math.random();
- 
+
       if (numeroAleatorio >= 0.5) return numero1;
       return numero2;
   }
@@ -70,7 +74,48 @@ const numeroEscolhido = chooseNumber(10, 20, "greater");
 
 console.log("Numero Escolhido", numeroEscolhido);
 // se a função retornar vazio escreve o valor void!
-function somar(num1:number, num2: number,): number{
-  return num1 + num2
-
+function somar(num1: number, num2: number): number {
+  return num1 + num2;
 }
+
+// * Utility Types: cria novos tipos a partir de tipos ja existentes.
+
+// 1. Parcial
+
+type PersonPartial = Partial<Person>;
+
+// 2. Required
+
+type PersonRequired = Required<Person>;
+
+// 3. Pick
+
+type Personpiked = Pick<Person, "nome" | "idade">;
+
+// 4. Omit
+
+type PersonOmit = Omit<Person, "profissao">;
+
+// 5. Exclude
+
+type criterioExclude = Exclude<Criterio, "greater">;
+
+// 6. Record
+
+type Pessoas = Record<string, Person>;
+
+// ex:
+
+const pessoas: Pessoas = {
+  "122.345.673-50": {
+    nome: "fulano",
+    idade: 20,
+    altura: 1.55,
+  },
+
+  "144.674.718-69": {
+    nome: "James",
+    idade: 34,
+    altura: 1.9,
+  },
+};
