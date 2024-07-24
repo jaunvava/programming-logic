f36aba51295399f53ddde244d37d2adc1b2148b8
jaunvava <mortalteck1020@gmail.com>
2024-07-24 10:41:49 -0300

    update script

--- projeto-todo-list/src/index.js
+++ projeto-todo-list/src/index.js
@@ -7,7 +7,81 @@ const todoListUl = document.querySelector("#todo-list-ul");
 // array 01
 let tasks = [];
 
-// código
+// function que add o html na pagina
+function rendertaskOnHtml(taskTitle, done = false) {
+  // adicionando nova tarefa no html ||
+  const li = document.createElement("li");
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.addEventListener("change", (event) => {
+    const liToToggle = event.target.parentElement;
+
+    const spanToToggle = liToToggle.querySelector("span");
+
+    const done = event.target.checked;
+    if (done) {
+      spanToToggle.style.textDecoration = "line-through";
+    } else {
+      spanToToggle.style.textDecoration = "none";
+    }
+
+    tasks = tasks.map((t) => {
+      if (t.title === spanToToggle.textContent) {
+        return {
+          title: t.title,
+          done: !t.done,
+        };
+      }
+
+      return t;
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  input.checked = done;
+
+  const span = document.createElement("span");
+  span.textContent = taskTitle;
+
+  if (done) {
+    span.style.textDecoration = "line-through";
+  }
+
+  const button = document.createElement("button");
+  button.textContent = "Remover";
+  button.addEventListener("click", (event) => {
+    const liToRemove = event.target.parentElement;
+
+    const titleToRemore = liToRemove.querySelector("span").textContent; //tarefa 1
+
+    tasks = tasks.filter((t) => t.title !== titleToRemore);
+
+    todoListUl.removeChild(liToRemove);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    // console.log(tasks);
+  });
+
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  todoListUl.appendChild(li);
+  // adicionando nova tarefa no html ||
+}
+
+window.onload = () => {
+  const taskOnLocalStorage = localStorage.getItem("tasks");
+  if (!taskOnLocalStorage) return;
+  tasks = JSON.parse(taskOnLocalStorage);
+
+  tasks.forEach((t) => {
+    rendertaskOnHtml(t.title, t.done);
+  });
+  // console.log(taskOnLocalStorage);
+};
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -19,24 +93,14 @@ form.addEventListener("submit", (event) => {
   }
 
   // adicionando novas tarefas detro do array de tasks
-  tasks.push(taskTitle);
-  
-  // adicionando nova tarefa no html
-  const li = document.createElement("li");
+  tasks.push({
+    title: taskTitle,
+    done: false,
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
-  const input = document.childElement("input");
-  input.setAttribute("type", "checkbox");
+  // chamando a function do html
+  rendertaskOnHtml(taskTitle);
 
-  const span = document.createElement("span");
-  span.textContent = taskTitle;
-
-  const button = document.createElement("button");
-  button.textContent = "Remover";
-
-  li.appendChild(input);
-  li.appendChild(span);
-  li.appendChild(button);
-
-  todoListUl.appendChild(li);
   textTitleInput.value = "";
 });
