5b9d43b523da1e3adbcffd6603f4c654e17555ef
jaunvava <mortalteck1020@gmail.com>
2024-07-23 23:35:14 -0300

    update

--- projeto-todo-list/src/index.js
+++ projeto-todo-list/src/index.js
@@ -4,9 +4,10 @@ const form = document.querySelector("#todo-form");
 const textTitleInput = document.querySelector("#task-title-input");
 // const addBotaoTask=document.querySelector('#add-botao-task')
 const todoListUl = document.querySelector("#todo-list-ul");
-
+// array 01
 let tasks = [];
 
+// código
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -19,6 +20,23 @@ form.addEventListener("submit", (event) => {
 
   // adicionando novas tarefas detro do array de tasks
   tasks.push(taskTitle);
+  
   // adicionando nova tarefa no html
-  todoListUl.appendChild()
+  const li = document.createElement("li");
+
+  const input = document.childElement("input");
+  input.setAttribute("type", "checkbox");
+
+  const span = document.createElement("span");
+  span.textContent = taskTitle;
+
+  const button = document.createElement("button");
+  button.textContent = "Remover";
+
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  todoListUl.appendChild(li);
+  textTitleInput.value = "";
 });
