ac312a65815e29e72fca771338ad2fbf9de02fd1
jaunvava <mortalteck1020@gmail.com>
2024-07-25 15:06:12 -0300

    fim

--- projeto-informações-clima/Js/index.js
+++ projeto-informações-clima/Js/index.js
@@ -11,17 +11,36 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const form = document.querySelector("#search-form > form");
 const input = document.querySelector("#input-localizacao ");
+const sectionTempoInfo = document.querySelector("#tempo-info");
 // eventos
 form === null || form === void 0 ? void 0 : form.addEventListener("submit", (event) => __awaiter(void 0, void 0, void 0, function* () {
     event.preventDefault();
-    if (!input)
+    if (!input || !sectionTempoInfo)
         return;
     const localizacao = input.value;
     if (localizacao.length < 3) {
         alert("O local Precisa ter, pelo menos, 3 letras");
         return;
     }
-    const response = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=e06329eac585b1d59cc630ab7f7b4729&lang=pt_br&units=metric`);
-    const dados = yield response.json();
-    console.log(dados);
+    // renderização
+    try {
+        const response = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=e06329eac585b1d59cc630ab7f7b4729&lang=pt_br&units=metric`);
+        const dados = yield response.json();
+        const infos = {
+            temperatura: Math.round(dados.main.temp),
+            local: dados.name,
+            icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`,
+        };
+        sectionTempoInfo.innerHTML = `
+            <div class="tempo-dados">
+              <h2>${infos.local}</h2>
+              <span>${infos.temperatura} ℃</span>
+            </div>
+    
+            <img src=${infos.icone} />`;
+    }
+    catch (err) {
+        console.log("Deu um erro na obtenção dos dados da API ", err);
+    }
 }));
+// * Fim
--- projeto-informações-clima/src/ts/index.ts
+++ projeto-informações-clima/src/ts/index.ts
@@ -4,13 +4,14 @@ const form = document.querySelector("#search-form > form");
 const input: HTMLInputElement | null = document.querySelector(
   "#input-localizacao "
 );
+const sectionTempoInfo = document.querySelector("#tempo-info");
 
 // eventos
 
 form?.addEventListener("submit", async (event) => {
   event.preventDefault();
 
-  if (!input) return;
+  if (!input || !sectionTempoInfo) return;
 
   const localizacao = input.value;
 
@@ -19,9 +20,28 @@ form?.addEventListener("submit", async (event) => {
     return;
   }
 
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=e06329eac585b1d59cc630ab7f7b4729&lang=pt_br&units=metric`
-  );
-  const dados = await response.json();
-  console.log(dados);
+  // renderização
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=e06329eac585b1d59cc630ab7f7b4729&lang=pt_br&units=metric`
+    );
+    const dados = await response.json();
+    const infos = {
+      temperatura: Math.round(dados.main.temp),
+      local: dados.name,
+      icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`,
+    };
+    sectionTempoInfo.innerHTML = `
+            <div class="tempo-dados">
+              <h2>${infos.local}</h2>
+              <span>${infos.temperatura} ℃</span>
+            </div>
+    
+            <img src=${infos.icone} />`;
+  } catch (err) {
+    console.log("Deu um erro na obtenção dos dados da API ", err);
+  }
 });
+
+// * Fim
